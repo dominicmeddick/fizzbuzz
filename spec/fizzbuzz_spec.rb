c8855892f8a24613e0e64cc57de2ff7a659979ce
Kerri McMahon <kerrimcm25@gmail.com>
2021-03-25 21:37:46 +0000

    Final RSpec and Fizzbuzz program

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,18 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed number divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed number divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed number divisible by both 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns itself when passed number indivisible by 3 and 5' do
+    expect(fizzbuzz(32)).to eq 32
+  end
+  it 'returns "fizzbuzz" when passed number divisible by both 3 and 5' do
+    expect(fizzbuzz(90)).to eq 'fizzbuzz'
+  end 
 end
-
